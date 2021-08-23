bdb5118fd5f13a1580735a3eafe9d4d05c2c4342
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2021-08-23 11:45:14 +0600

    Create Input and Output.cpp

---
+++ C++ solutions/Input and Output.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d", a+b+c);
+    return 0;
+}
