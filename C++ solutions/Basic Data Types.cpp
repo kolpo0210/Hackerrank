a64dba75866e5543b6c09308ff32543a9a5aa550
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2021-08-23 11:47:13 +0600

    Create Basic Data Types.cpp

---
+++ C++ solutions/Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    long i, j;
+    float k;
+    double l;
+    char ch;
+    
+    cin >> i >> j >> ch >> k >> l;
+    cout << i << "\n" << j << "\n" << ch << "\n";
+    printf("%f\n", k);
+    printf("%lf", l);
+    
+    return 0;
+}
