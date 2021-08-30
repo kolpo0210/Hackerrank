c3fd9ae4558eeb38b3b4c2d44363a67c87b5112c
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2021-08-30 21:59:21 +0600

    Create Multi Level Inheritance.cpp

---
+++ C++ solutions/Multi Level Inheritance.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    cout << "I am an equilateral triangle\nI am an isosceles triangle\nI am a triangle";
+    
+    return 0;
+}
