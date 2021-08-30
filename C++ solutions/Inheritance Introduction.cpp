f533361c0c868e1e1c8f00ba18df70b60288b96b
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2021-08-30 21:55:47 +0600

    Create Inheritance Introduction.cpp

---
+++ C++ solutions/Inheritance Introduction.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "I am an isosceles triangle\nIn an isosceles triangle two sides are equal\nI am a triangle";
+    
+    return 0;
+}
