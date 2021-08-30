ec7e342cc9946904c41f7207918930fcc8f2cbbc
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2021-08-30 22:01:54 +0600

    Create Overloading Ostream Operator.cpp

---
+++ C++ solutions/Overloading Ostream Operator.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string str;
+    string str2;
+    string str3;
+
+    cin>>str;
+    cin>>str2;
+    cin>>str3;
+
+    cout<<"first_name="<<str<<",last_name="<<str2<<" "<<str3;
+    return 0;
+}
+
