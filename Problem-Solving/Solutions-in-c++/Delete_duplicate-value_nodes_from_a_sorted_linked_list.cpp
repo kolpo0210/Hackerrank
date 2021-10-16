b90e01642af77cf8bfabf7b33a23956d0ffe3aa2
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2021-10-16 19:34:25 +0600

    linked list files upload

--- Problem-Solving/Solutions-in-c++/Delete_duplicate-value_nodes_from_a_sorted_linked_list.cpp
+++ Problem-Solving/Solutions-in-c++/Delete_duplicate-value_nodes_from_a_sorted_linked_list.cpp
@@ -1,34 +1,78 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<cstdio>
+#include<cstdlib>
 using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node* RemoveDuplicates(Node *head)
+{
+    Node *current=head,*temp;
+    if(head == NULL)return head;
+    if(head->next == NULL) return head;
+    else
+    {
+        while(current->next != NULL)
+        {
+
+            if(current->data == current->next->data)
+            {
+                temp = current->next->next;
+                current->next = temp;
+            }
+            else
+                current=current->next;
+        }
+        return head;
+    }
+
+}
+
+void Print(Node *head)
+{
+    bool ok = false;
+    while(head != NULL)
+    {
+        if(ok)cout<<" ";
+        else ok = true;
+        cout<<head->data;
+        head = head->next;
+    }
+    cout<<"\n";
+}
+Node* Insert(Node *head,int x)
+{
+    Node *temp = new Node();
+    temp->data = x;
+    temp->next = NULL;
+    if(head == NULL)
+    {
+        return temp;
+    }
+    Node *temp1;
+    for(temp1 = head; temp1->next!=NULL; temp1= temp1->next);
+    temp1->next = temp;
+    return head;
+}
 int main()
 {
-    int n,t;
-    int list_array[1000];
-    int temp=0;
-
-    cin>>n>>t;
-
-    for(int i=0; i<t; i++)
+    int t;
+    cin>>t;
+    while(t-- >0)
     {
-        for(int j=0; j<n; j++)
+        Node *A = NULL;
+        int m;
+        cin>>m;
+        while(m--)
         {
-            cin>>list_array[j];
+            int x;
+            cin>>x;
+            A = Insert(A,x);
         }
-        for(int j=0; j<n; j++)
-        {
-            for(int k=j+1; k<n; k++)
-            {
-                if(list_array[k] > list_array[j])
-                {
-                    temp = list_array[j];
-                    list_array[j] = list_array[k];
-                    list_array[k] = temp;
-                }
-            }
-        }
-        for(int j=0; j<n; j++)
-        {
-            cout<<list_array[j]<<' ';
-        }
+        A = RemoveDuplicates(A);
+        Print(A);
     }
 }
